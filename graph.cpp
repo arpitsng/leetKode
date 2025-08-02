6b74fc69ba863386cf4b08b745cb22ddbf82ad6f
arpitsng <paemjeetk@gmail.com>
2025-08-03 00:04:47 +0530

    question added

--- graph.cpp
+++ graph.cpp
@@ -57,3 +57,33 @@ int main()
 
     return 0;
 }
+
+//coding ninjas quess-- print and create adjacency list
+// vector < vector < int >> printAdjacency(int n, int m, vector < vector < int >> & edges) {
+
+//     vector<int> ans[n];  //square brackets means vector<vector<int>> of n size
+
+//     for(int i=0; i<m; i++){
+
+//         int u = edges[i][0];
+//         int v = edges[i][1];
+
+//         ans[u].push_back(v);
+//         ans[v].push_back(u);
+
+//     }
+
+//     vector<vector<int>> adj(n);
+//     for(int i=0; i<n; i++){
+//         adj[i].push_back(i); //as per output formmat//
+
+//         //entering neighbours
+//         for(int j=0; j<ans[i].size(); j++){
+//             adj[i].push_back(ans[i][j]);
+//         }
+
+
+//     }
+
+//     return adj;
+// }
